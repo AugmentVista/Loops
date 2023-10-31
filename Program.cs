5c66775a5654fa4c704aac58bc8d22b4c9634930
AugmentVista <AuAgments@gmail.com>
2023-10-31 14:55:40 -0300

    Loops

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("MiniGame");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+            //beans
+        }
+    }// beans
+}
