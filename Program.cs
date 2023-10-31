038bc154e7bd6543f9513c7914dd089b8206c6cf
AugmentVista <AuAgments@gmail.com>
2023-10-31 15:20:37 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -8,16 +8,34 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            char avatar = 'P';
 
-
-
-            Console.WriteLine("MiniGame");
+            for (int i = 1; i <= 100; i++) //<-- shortcut
+                Console.WriteLine("MiniGame");
             Console.WriteLine();
             Console.WriteLine();
 
             Console.ReadKey();
+            bool gameOver = false;
+            while (gameOver != true)
+            {
+             //....
 
-            //beans
+            // turn states
+            }
+            ConsoleKeyInfo input;
+            input = Console.ReadKey();
+            if (input.Key == ConsoleKey.W)
+            {
+                Console.WriteLine(avatar);
+                //if (y < 0) y = 0;
+                // WASD controls 
+                // [y,x]
+                // y++;
+                // x = x -1;
+            }
+
+            //KNOWLEDGE
         }
-    }// beans
+    }
 }
